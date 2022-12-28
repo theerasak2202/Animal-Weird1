d83d68d63b002e04759638eec2574ef80b5043c0
Theerasak Yongen <121445197+theerasak2202@users.noreply.github.com>
2022-12-28 22:13:43 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
           <div>
             <a
        
-              window.location.href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
             >
