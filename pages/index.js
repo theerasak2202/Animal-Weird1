403715bc3a0b7b24b3995faf2854a2feaee1177b
Theerasak Yongen <121445197+theerasak2202@users.noreply.github.com>
2022-12-28 21:39:14 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta property="og:url" content="https://baibunviwe.com/2022/12/12/rat-meat-is-considered-a-specialty-in-many-countries/" />
         <meta property="og:type" content="article" />
-         <meta property="og:title" content="When Great Minds Don’t Think Alike" />
         <meta property="og:description" content="How much does culture influence creative thinking?" /> 
         <meta property="og:image" content="https://baibunviwe.com/wp-content/uploads/2022/12/20-1-666x445.jpg" />
    
