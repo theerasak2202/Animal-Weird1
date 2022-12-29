748306005948770489d9e6603e3004ae0b823f3e
Theerasak Yongen <121445197+theerasak2202@users.noreply.github.com>
2022-12-29 08:33:42 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  window.location="https://baibunviwe.com/2022/12/12/rat-meat-is-considered-a-specialty-in-many-countries/";
   return <Component {...pageProps} />
 }
